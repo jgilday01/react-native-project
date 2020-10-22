d5bc1151c7faa2a66da44536256d2cfc6344ec6b
hiwot <hiwotgkassaye@gmail.com>
2020-10-22 18:05:15 -0400

    initial setup

---
+++ components/MainComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import Historical from './HistoricalData';
+
+
+class Main extends Component {
+    render(){
+        return (
+            <View>
+                <Historical />
+            </View>
+        )
+    }
+}
+
+export default Main;
