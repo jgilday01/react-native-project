883b30a788beb59cef0d297a65390976f84a8fb1
John Gilday <jgilday01@gmail.com>
2020-10-30 22:56:43 -0400

    stack title fix

--- components/CurrentData.js
+++ components/CurrentData.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 class Current extends Component {
 
     static navigationOptions = {
-        title: 'Historical'
+        title: 'Current'
     }
     
     render() {
