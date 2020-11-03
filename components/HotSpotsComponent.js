9342bf4adaeb00be95661d6ecca72ad937ad6276
John Gilday <jgilday01@gmail.com>
2020-11-03 08:04:08 -0500

    new hotspots

--- components/HotSpotsComponent.js
+++ components/HotSpotsComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Text, FlatList, View, Dimensions } from 'react-native';
-import { LineChart } from "react-native-chart-kit";
+import { BarChart } from "react-native-chart-kit";
 
 class Home extends Component {
     constructor(props) {
@@ -11,10 +11,10 @@ class Home extends Component {
         };
     }
 
-    static navigationOptions = { title: 'Home' }
+    static navigationOptions = { title: 'HotSpot' }
 
     componentDidMount() {
-        fetch('https://api.covidtracking.com/v1/us/daily.json')
+        fetch('https://api.covidtracking.com/v1/states/current.json')
             .then(response => response.json())
             .then(result => {
                 this.setState({ loading: false, items: result });
@@ -24,17 +24,20 @@ class Home extends Component {
 
     render() {
 
-        const points = this.state.items.map(item => item.death / 1000).slice(0, 7).reverse();
+        const sortItems = this.state.items.sort((a, b) => (a.positiveIncrease > b.positiveIncrease)
+            ? 1 : (a.positiveIncrease === b.positiveIncrease) ? ((a.positiveIncrease > b.positiveIncrease) ? 1 : -1) : -1);
 
-        const labels = this.state.items.map(item => item.dateChecked.slice(5, 10) ).slice(0, 7).reverse();
+        sortItems.reverse();
+
+        const points = sortItems.map(item => item.positiveIncrease).slice(0, 5).reverse();
+
+        const labels = sortItems.map(item => item.state).slice(0, 5).reverse();
 
         const linedata = {
             labels: labels,
             datasets: [
                 {
-                    data: points,
-                    color: (opacity = 1) => `rgba(250, 88, 88, ${opacity})`,
-                    strokeWidth: 5// optional
+                    data: points
                 }
             ]
         };
@@ -42,20 +45,20 @@ class Home extends Component {
         const renderDataItem = ({ item }) => {
             return (
                 <View style={{ flex: 1, borderRadius: 10, backgroundColor: "#333", padding: 15, margin: 15 }}>
-                    <Text style={{ color: "#e55", fontSize: 20, textAlign: "center"}}>Deaths: {item.death}</Text>
+                    <Text style={{ color: "#e55", fontSize: 20, textAlign: "center" }}>Highest State: {item.state}</Text>
+                    <View style={{
+                        height: 2, margin: 5,
+                        backgroundColor: 'rgba(222, 222, 222 ,0.5)',
+                        alignSelf: 'stretch'
+                    }} />
+                    <Text style={{ color: "#eee", fontSize: 18, textAlign: "center" }}>Positive Increase: {item.positiveIncrease}</Text>
                     <View style={{
                         height: 2, margin: 5,
                         backgroundColor: 'rgba(222, 222, 222 ,0.5)',
                         alignSelf: 'stretch'
                     }} />
                     <Text style={{ color: "#eee", fontSize: 18, textAlign: "center" }}>Positive: {item.positive}</Text>
-                    <Text style={{ color: "#eee", fontSize: 18, textAlign: "center" }}>Hospitalized: {item.hospitalized}</Text>
-                    <View style={{
-                        height: 2, margin: 5,
-                        backgroundColor: 'rgba(222, 222, 222 ,0.5)',
-                        alignSelf: 'stretch'
-                    }} />
-                    <Text style={{ color: "#eee", fontSize: 20, textAlign: "center" }}>Recovered: {item.recovered}</Text>
+                    <Text style={{ color: "#eee", fontSize: 18, textAlign: "center" }}>Negative: {item.negative}</Text>
                 </View>
             )
         }
@@ -65,19 +68,20 @@ class Home extends Component {
         } else {
             return (
                 <View style={{ flex: 1, backgroundColor: '#222222' }}>
-                    <Text style={{ color: "#F77", textAlign: "center", fontSize: 33, marginTop: 10 }}>US death stats</Text>
-                    <LineChart
+                    <Text style={{ color: "#FEE", textAlign: "center", fontSize: 33, marginTop: 10 }}>High Positive Increase</Text>
+                    <BarChart
                         style={{ margin: 15, borderRadius: 5 }}
                         data={linedata}
-                        width={Dimensions.get('window').width}
-                        verticalLabelRotation={90}
-                        chartConfig={{          
-                            color: (opacity = 1) => `rgba(250, 250, 250, ${opacity})`,
-                            decimalPlaces: 1
+                        height={325}
+                        width={Dimensions.get('window').width - 30} 
+                        chartConfig={{
+                            
+                            backgroundGradientFrom: '#A00',
+                            backgroundGradientTo: '#A55',
+                            color: (opacity = 1) => `rgba(250, 250, 131, ${opacity})`,
+                            decimalPlaces: 0
 
                         }}
-                        yAxisSuffix="k"
-                        height={325}
                     />
                     <FlatList
                         data={this.state.items.slice(0, 1)}
